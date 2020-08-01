17699abbdd8f45540ede031371bfe68b5a447766
Jonathan Wears <jonathanwears@outlook.com>
2020-08-01 23:18:00 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as Comp from './computation.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+  const [input, setInput] = useState("");
+  const [operator, setOperator] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  
+  function condition(input) {
+
+      const errorMessages = {
+        'noOperator': "Error: There is no operator!",
+        'NaN': "Not enough numbers (NaN)"
+      }
+
+
+    let nums = input;
+     /* 
+      if (input === null) {
+        setInput("");
+        setErrorMessage(errorMessages.NaN);
+        
+      }
+        else if(input.includes("+") !== true||input.includes("-") !== true||input.includes("*") !== true||input.includes("/") !== true) {
+          setErrorMessage(errorMessages.noOperator)
+          setInput("");
+          
+          }else {
+                
+          console.log("after error")
+*/
+          let num1 = parseInt(nums.split(operator).shift());
+          let num2 = parseInt(nums.split(operator).pop());
+
+          switch(operator) {
+
+            case "+": 
+              setInput(Comp.addition(num1, num2));
+              break;
+            case "-":
+              setInput(Comp.subtract(num1, num2));
+              break;
+            case "*":
+              setInput(Comp.multiply(num1, num2));
+              break;
+            case "/":
+              setInput(Comp.divide(num1, num2));
+            break;
+          }
+      
+        }
+
+      function handleNumClick(e) {
+
+        const operations = {
+          '+': Math.addition,
+        };
+
+        const value = e.target.value;
+
+
+        if(value.includes('=')) {
+            condition(input)
+
+          }else if(value.includes("AC")) {
+
+            setInput("");
+
+            }else if(value.includes("+")||value.includes("-")||value.includes("*")||value.includes("/") ) {
+
+              setOperator(value);
+              setInput(input + value);
+
+              }else {
+              setInput(input + value)
+            }
+      
+  }
+  
+      return ( 
+        <div className="App">
+          
+
+          {[0,1,2,3,4,5,6,7,8,9,"+","-","*","/","=","AC"].map(i =>
+          
+          <button 
+          value= {i}
+          onClick={handleNumClick} 
+          >
+          {i}
+          </button>
+  
+          )}  
+
+      <p>{input}</p>
+      <p>{errorMessage}</p>
+        </div>
+      );
+        
 }
 
 export default App;
---
+++ src/computation.js
@@ -0,0 +1,24 @@
+export function hello() {
+        // This is here only to test the import/export is working
+        console.log("hello from computation");
+};
+
+export function addition(num1, num2) {
+        
+    return num1 + num2;
+};
+
+export function subtract(num1, num2) {
+    
+    return num1 - num2;
+};
+
+export function multiply(num1, num2) {
+    
+    return num1 * num2;
+};
+
+export function divide(num1, num2) {
+        
+    return num1 / num2;
+};
