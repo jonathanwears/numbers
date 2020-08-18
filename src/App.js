e39ab08c016d8e36a64fc3f7070e11569b7d533e
Jonathan Wears <jonathanwears@outlook.com>
2020-08-18 18:05:44 +0200

    Added CSS

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ function App(props) {
   return ( 
         <div className="App">
           
-          {[0,1,2,3,4,5,6,7,8,9,"+","-","*","/","=","AC"].map(i =>
+          {[1,2,3,4,5,6,7,8,9,0,"+","-","*","/","=","AC"].map(i =>
           
           <button 
           value= {i}
